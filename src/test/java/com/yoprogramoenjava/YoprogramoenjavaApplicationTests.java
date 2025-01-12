1d7a596781bd8ee221c0eaed527516ce940bc3d4
Enrique Sanchez <esanchez.devel@gmail.com>
2025-01-12 12:52:22 +0100

    feat: project creation

---
+++ src/test/java/com/yoprogramoenjava/YoprogramoenjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoprogramoenjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoprogramoenjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
