4455c12a266390375798762d5c1edae6bc748d50
Enrique Sanchez <esanchez.devel@gmail.com>
2025-01-12 20:45:47 +0100

    feat: create index page with css styles in web title

---
+++ src/main/java/com/yoprogramoenjava/application/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.yoprogramoenjava.application.utils;
+
+public class Constants {
+
+	public static final String ATTRIBUTE_NAME_TITLE = "title";
+	
+	public static final String ATTRIBUTE_VALUE_TITLE = "Yo programo en Java";
+}
--- src/main/java/com/yoprogramoenjava/infrastructure/controller/FrontendController.java
+++ src/main/java/com/yoprogramoenjava/infrastructure/controller/FrontendController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.yoprogramoenjava.application.utils.Constants;
+
 @Controller
 public class FrontendController {
 
 	@GetMapping("/")
 	public String getIndex(Model model) {
+		
+		model.addAttribute(Constants.ATTRIBUTE_NAME_TITLE, Constants.ATTRIBUTE_VALUE_TITLE);
+		
 		return "index";
 	}
 }
