fff1e79e401960f54ffe54ed7b5facf7ae434c9e
Enrique Sanchez <esanchez.devel@gmail.com>
2025-01-18 20:17:35 +0100

    feat: create table of articles in admin panel

--- src/main/java/com/yoprogramoenjava/infrastructure/controller/AdminController.java
+++ src/main/java/com/yoprogramoenjava/infrastructure/controller/AdminController.java
@@ -5,12 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.yoprogramoenjava.application.utils.Constants;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
 	@GetMapping() 
 	public String getAdminPanel(Model model) {
+		model.addAttribute(Constants.ATTRIBUTE_NAME_TITLE, Constants.ATTRIBUTE_VALUE_TITLE);
+		
 		return "admin/index";
+	}
+	
+	@GetMapping("/articles")
+	public String getArticles(Model model) {
+		model.addAttribute(Constants.ATTRIBUTE_NAME_TITLE, Constants.ATTRIBUTE_VALUE_TITLE);
+		
+		return "admin/articles";
 	}
 }
