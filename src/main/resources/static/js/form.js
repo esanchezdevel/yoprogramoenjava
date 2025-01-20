855c24b80a101a0cec2ee3d02c24b27d1db5d944
Enrique Sanchez <esanchez.devel@gmail.com>
2025-01-20 20:47:38 +0100

    feat: add button to add italic tags in article content

--- src/main/resources/static/js/form.js
+++ src/main/resources/static/js/form.js
@@ -1,26 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const content = document.getElementById('content');
 	const button_bold = document.getElementById('button-bold');
+	const button_italic = document.getElementById('button-italic');
 	
 	button_bold.addEventListener('click', (event) => {
 		console.log("bold button clicked")
 		event.preventDefault();
-		
-	    const start = content.selectionStart;
-	    const end = content.selectionEnd;
-	
-	    const text = content.value;
-	    const selected_text = text.substring(start, end);
-	
-	    if (selected_text) {
-	        content.value = text.substring(0, start) + `[b]${selected_text}[/b]` + text.substring(end);
-	    } else {
-	        content.value = text.substring(0, start) + `[b][/b]` + text.substring(start);
-	        // Move the tags to the center between both tags [b][/b]
-	        content.selectionStart = content.selectionEnd = start + 3;
-	    }
-	
-		// Focus again the text area to continue editing
-	    content.focus();
+		add_tags(content, 'b')
 	});
+
+	button_italic.addEventListener('click', (event) => {
+		console.log("bold italic clicked")
+		event.preventDefault();
+		add_tags(content, 'i')
+	});
+		
+	function add_tags(content, tag) {
+		const start = content.selectionStart;
+		const end = content.selectionEnd;
+
+		const text = content.value;
+		const selected_text = text.substring(start, end);
+
+		if (selected_text) {
+		    content.value = text.substring(0, start) + `[` + tag + `]${selected_text}[/` + tag + `]` + text.substring(end);
+		} else {
+		    content.value = text.substring(0, start) + `[` + tag + `][/` + tag + `]` + text.substring(start);
+		    // Move the tags to the center between both tags
+		    content.selectionStart = content.selectionEnd = start + 3;
+		}
+
+		// Focus again the text area to continue editing
+		content.focus();
+	}
 });
