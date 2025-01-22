48059f498eb98b916f1076b3a387849586a02a5c
Enrique Sanchez <esanchez.devel@gmail.com>
2025-01-22 20:20:54 +0100

    feat: add csrf input to form

--- src/main/java/com/yoprogramoenjava/infrastructure/controller/AdminController.java
+++ src/main/java/com/yoprogramoenjava/infrastructure/controller/AdminController.java
@@ -1,9 +1,12 @@
 package com.yoprogramoenjava.infrastructure.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yoprogramoenjava.application.utils.Constants;
@@ -13,6 +16,8 @@ import com.yoprogramoenjava.domain.service.ArticlesService;
 @RequestMapping("/admin")
 public class AdminController {
 
+	private static final Logger logger = LogManager.getLogger(AdminController.class);
+	
 	@Autowired
 	private ArticlesService articlesService;
 	
@@ -38,4 +43,12 @@ public class AdminController {
 		
 		return "admin/article_form";
 	}
+	
+	@PostMapping("/articles/create")
+	public String postCreateArticle(Model model) {
+		logger.info("Processing new article");
+		return "admin/index";
+	}
+	
+	
 }
