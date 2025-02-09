ae8d814a6967fcb510cd4999bca8d54bc4106d0f
Enrique Sanchez <esanchez.devel@gmail.com>
2025-02-09 13:46:08 +0100

    refactor: rename users repository

--- src/main/java/com/yoprogramoenjava/infrastructure/db/repository/UsersRepository.java
+++ src/main/java/com/yoprogramoenjava/infrastructure/db/repository/UsersRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yoprogramoenjava.domain.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends JpaRepository<User, Long> {
 	
 }
