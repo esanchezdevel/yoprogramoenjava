675d727a8c0e35717617bc96f4ca291e09ba9fa6
Enrique Sanchez <esanchez.devel@gmail.com>
2025-02-15 10:48:39 +0100

    feat: implement ExternalNews update method

--- src/main/java/com/yoprogramoenjava/domain/service/impl/ExternalNewsServiceImpl.java
+++ src/main/java/com/yoprogramoenjava/domain/service/impl/ExternalNewsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.yoprogramoenjava.domain.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +13,8 @@ import com.yoprogramoenjava.application.exception.AppException;
 import com.yoprogramoenjava.domain.model.ExternalNew;
 import com.yoprogramoenjava.domain.service.ExternalNewsService;
 import com.yoprogramoenjava.infrastructure.db.repository.ExternalNewsRepository;
+
+import jakarta.transaction.Transactional;
 
 public class ExternalNewsServiceImpl implements ExternalNewsService {
 
@@ -51,9 +54,47 @@ public class ExternalNewsServiceImpl implements ExternalNewsService {
 
 
 	@Override
+	@Transactional
 	public void update(Long id, ExternalNew externalNew) throws AppException {
-		// TODO Auto-generated method stub
+		if (!isValid(externalNew)) {
+			String errorMsg = new StringBuilder("Error. Wrong ExternalNew object received: ").append(externalNew).toString();
+			logger.error(errorMsg);
+			throw new AppException(HttpStatus.BAD_REQUEST.value(), errorMsg);
+		}
 		
+		ExternalNew externalNewDbValue = null;
+		try {
+			Optional<ExternalNew> externalNewDb = externalNewsRepository.findById(id);
+
+			if (externalNewDb.isEmpty()) {
+				String errorMsg = new StringBuilder("Error. ExternalNew with id '").append(id).append("' not found").toString();
+				logger.error(errorMsg);
+				throw new AppException(HttpStatus.NOT_FOUND.value(), errorMsg);
+			}
+
+			externalNewDbValue = externalNewDb.get();
+		} catch (Exception e) {
+			if (e instanceof AppException) {
+				throw e;
+			} else {
+				String errorMsg = new StringBuilder("Error. Unexpected error happens getting ExternalNew with id '")
+														.append(id).append("' from Database").toString();
+				logger.error(errorMsg);
+				throw new AppException(HttpStatus.NOT_FOUND.value(), errorMsg);
+			}
+		}
+
+		try {
+			externalNewDbValue.setTitle(externalNew.getTitle());
+			externalNewDbValue.setSource(externalNew.getSource());
+			externalNewDbValue.setLink(externalNew.getLink());
+		} catch (Exception e) {
+			String errorMsg = new StringBuilder("Error updating ExternalNew with id '")
+													.append(id).append("': ").append(externalNew).append(". ")
+													.append(e.getMessage()).toString();
+			logger.error(errorMsg, e);
+			throw new AppException(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorMsg);
+		}
 	}
 
 
