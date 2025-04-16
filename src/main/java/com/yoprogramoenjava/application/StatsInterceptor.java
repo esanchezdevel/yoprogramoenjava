7ba4abe53b0b8383a5bd3b24b57dd07bc1f8d814
Enrique Sanchez <esanchez.devel@gmail.com>
2025-04-16 20:55:39 +0200

    feat: filter the bots from the stats before save it

--- src/main/java/com/yoprogramoenjava/application/StatsInterceptor.java
+++ src/main/java/com/yoprogramoenjava/application/StatsInterceptor.java
@@ -1,5 +1,7 @@
 package com.yoprogramoenjava.application;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -16,6 +18,9 @@ public class StatsInterceptor implements HandlerInterceptor {
 	@Autowired
     private StatsRepository statsRepository;
 
+    private static final List<String> BOTS_IDENTIFIERS = List.of("Googlebot", "Bingbot", "Slurp", "DuckDuckBot",
+            "Baiduspider", "YandexBot", "Sogou", "Exabot", "facebot", "ia_archiver");
+
     @SuppressWarnings("null")
 	@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
@@ -24,15 +29,26 @@ public class StatsInterceptor implements HandlerInterceptor {
 		String method = request.getMethod();
         String endpoint = request.getRequestURI();
 
-        Stat stat = new Stat();
-        stat.setIp(ip);
-        stat.setUserAgent(userAgent);
-		stat.setMethod(method);
-        stat.setEndpoint(endpoint);
-
-        statsRepository.save(stat);
-
+        if (!isBot(userAgent)) {
+            Stat stat = new Stat();
+            stat.setIp(ip);
+            stat.setUserAgent(userAgent);
+            stat.setMethod(method);
+            stat.setEndpoint(endpoint);
+    
+            statsRepository.save(stat);
+        }
         return true;
     }
 
+    private boolean isBot(String userAgent) {
+        boolean isBot = false;
+        for (String identifier : BOTS_IDENTIFIERS) {
+            if (userAgent.contains(identifier)) {
+                isBot = true;
+                break;
+            }
+        }
+        return isBot;
+    }
 }
