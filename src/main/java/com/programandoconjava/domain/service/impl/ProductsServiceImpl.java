328827169321615aabb7f88dca5c019312766ee7
Enrique Sanchez <esanchez.devel@gmail.com>
2025-05-16 15:00:15 +0200

    refactor: remove unused import

--- src/main/java/com/programandoconjava/domain/service/impl/ProductsServiceImpl.java
+++ src/main/java/com/programandoconjava/domain/service/impl/ProductsServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.programandoconjava.domain.model.Article;
 import com.programandoconjava.domain.model.Product;
 import com.programandoconjava.domain.service.HtmlParserService;
 import com.programandoconjava.domain.service.ProductsService;
